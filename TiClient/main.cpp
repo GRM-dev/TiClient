eed8840ca69b5437d4fed86764085cae5c08fd6c
Mateusz Paluszkiewicz <aifam96@gmail.com>
2015-05-22 14:17:39 +0200

    First files

---
+++ TiClient/main.cpp
@@ -0,0 +1,15 @@
+// TiClient.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPTSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	return 0;
+}
